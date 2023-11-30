d720a3927a1281e50de331be177b6fcf2a941b4d
Kazuya Takahashi <electrolysis.jp@gmail.com>
2023-11-30 21:53:52 +0900

    Use extra-footer.html

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,6 @@ language = "ja"
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "show_toc_level": 2,
-    "extra_footer": '<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />この 作品 は <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">クリエイティブ・コモンズ 表示 4.0 国際 ライセンス</a>の下に提供されています。',
 }
 
 html_static_path = ["_static"]
